e5ef0f6c2b3514bcfc83bf2d00cae3515f63eb86
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2021-01-23 21:11:40 -0800

    integrate-logger - inject logger manager and update unit tests

--- SentryIntegration/Controllers/StatusController.cs
+++ SentryIntegration/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SentryIntegration.Interfaces;
 using SentryIntegration.Models;
 
 namespace SentryIntegration.Controllers 
@@ -6,10 +7,16 @@ namespace SentryIntegration.Controllers
     [ApiController]
     public class StatusController : ControllerBase
     {
+        private readonly ILoggerManager _logger;
 
+        public StatusController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
         [HttpGet("/api/v1/status")]
         public StatusResponse Get()
         {
+            _logger.LogInformation("Sending status back");
             return new StatusResponse { Status = "OK", Version = "0.1.0" };
         }
     }
--- SentryIntegrationTests/Controllers/StatusControllerTests.cs
+++ SentryIntegrationTests/Controllers/StatusControllerTests.cs
@@ -1,14 +1,19 @@
+using Moq;
 using SentryIntegration.Controllers;
+using SentryIntegration.Interfaces;
 using Xunit;
 
 namespace SentryIntegrationTests.Controllers 
 {
     public class StatusControllerTests
     {
+
+
         [Fact]
         public void ShouldReturnStatusOk()
         {
-            var sut = new StatusController();
+            var loggerMock = new Mock<ILoggerManager>();
+            var sut = new StatusController(loggerMock.Object);
             var response = sut.Get();
 
             Assert.Equal("OK", response.Status);
